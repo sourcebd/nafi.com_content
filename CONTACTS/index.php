3c9e27fde8e7212362101c4614a9c572313fa01d
Nafi Mahmud <nativetube71@gmail.com>
2020-11-13 23:14:06 +0600

    good committed

--- CONTACTS/index.php
+++ CONTACTS/index.php
@@ -5,8 +5,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NAFI.COM Contact Form</title>
-    <link rel="stylesheet" href="style.css?version=1.0">
+    <title>Contacts</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <style>
 a:link, a:visited {
