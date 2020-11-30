2762e28ed628e5b16ab6757bca0eaa7c7b018ea3
Nafi Mahmud <nativetube71@gmail.com>
2020-11-30 11:57:19 +0600

    Update Regdb.php

--- SIGN UP/Control/Regdb.php
+++ SIGN UP/Control/Regdb.php
@@ -14,10 +14,19 @@ class Regdb{
     
     function insertUser($conn,$fname,$lname,$password,$email,$gender,$profession,$date,$file)
     {
+        $stmt=$conn->prepare("insert into users(firstname,lastname,password,email,gender,profession,date,file) values(?,?,?,?,?,?,?,?)") or die(mysqli_error($conn));
+        $stmt->bind_param("ssssssss",$fname,$lname,$password,$email,$gender,$profession,$date,$file) or die(mysqli_error($conn));
+        $stmt->execute();
+        echo "<h3>Registration Successful!</h3>";
+        $stmt->close();
+    }
+
+    /*function insertUser($conn,$fname,$lname,$password,$email,$gender,$profession,$date,$file)
+    {
         $result=$conn->query("insert into users(firstname,lastname,password,email,gender,profession,date,file) values('$fname','$lname','$password','$email','$gender','$profession','$date','$file')") or die(mysqli_error($conn));
         return $result;
         echo "<h3>Registration Successful!</h3>";
-    }
+    }*/
     function CloseCon($conn)
      {
      $conn -> close();
